3bfc134c538e590e5b716726c9493366aa4b8647
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2023-12-29 23:57:18 +0100

    commit

---
+++ src/main/java/pl/crystaldpf/pogodnik/Controller.java
@@ -0,0 +1,47 @@
+package pl.crystaldpf.pogodnik;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private Button getData;
+
+    @FXML
+    private Text pressure;
+
+    @FXML
+    private Text temp_feels;
+
+    @FXML
+    private Text temp_info;
+
+    @FXML
+    private Text temp_max;
+
+    @FXML
+    private Text temp_min;
+
+    @FXML
+    void initialize() {
+    getData.setOnAction(actionEvent -> {
+        System.out.println("SUPER");
+    });
+
+    }
+
+}
--- src/main/java/pl/crystaldpf/pogodnik/HelloApplication.java
+++ src/main/java/pl/crystaldpf/pogodnik/HelloApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sample.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 251, 356);
+        stage.setTitle("Погодник");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
